6f948f768fda0f3d7a00650fc23a1e93923b4c95
Rachael Bond <962122+rachaelbond@users.noreply.github.com>
2023-04-04 10:04:50 +0100

    Update config_flow.py

--- custom_components/shopping_list_with_grocy/config_flow.py
+++ custom_components/shopping_list_with_grocy/config_flow.py
@@ -65,8 +65,8 @@ class ShoppingListWithGrocyOptionsConfigFlow(config_entries.OptionsFlow):  # typ
                         "mqtt_password", default=self.options.get("mqtt_password")
                     ): cv.string,
                     vol.Optional(
-                        "adding_images",
-                        default=self.options.get("adding_images", "0")
+                        "image_download_size",
+                        default=self.options.get("image_download_size", "0")
                     ): vol.All(cv.string, vol.In(["0", "100", "200", "400"])),
                     vol.Optional(
                         "adding_products_in_sensor",
@@ -130,7 +130,7 @@ class ShoppingListWithGrocyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     ),
                     vol.Required("mqtt_username"): cv.string,
                     vol.Required("mqtt_password"): cv.string,
-                    vol.Optional("adding_images", default="0"):
+                    vol.Optional("image_download_size", default="0"):
                         vol.All(cv.string, vol.In(["0", "100", "200", "400"])),
                     vol.Optional(
                         "adding_products_in_sensor", default=False
