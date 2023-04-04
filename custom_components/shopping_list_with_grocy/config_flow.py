6073221c2d4a5cf5f2f3a5874795744550490cfd
Anrolosia <bmoreau1711@gmail.com>
2023-04-04 10:56:57 -0400

    fix: Fixed issue raised in #16 with config flow

--- custom_components/shopping_list_with_grocy/config_flow.py
+++ custom_components/shopping_list_with_grocy/config_flow.py
@@ -130,9 +130,10 @@ class ShoppingListWithGrocyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     ),
                     vol.Required("mqtt_username"): cv.string,
                     vol.Required("mqtt_password"): cv.string,
-                    vol.Optional("image_download_size", default=0): vol.All(
-                        cv.int, vol.In([0, 100, 200, 400])
-                    ),
+                    vol.Optional(
+                        "image_download_size",
+                        default=self.options.get("image_download_size", 0),
+                    ): vol.All(cv.positive_int, vol.In([0, 100, 200, 400])),
                     vol.Optional(
                         "adding_products_in_sensor", default=False
                     ): cv.boolean,
