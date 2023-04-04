889d07d576db39a0cd924b48b0091dc310ec5b3c
Rachael Bond <962122+rachaelbond@users.noreply.github.com>
2023-04-04 12:25:17 +0100

    Update shopping_list_with_grocy.py

--- custom_components/shopping_list_with_grocy/apis/shopping_list_with_grocy.py
+++ custom_components/shopping_list_with_grocy/apis/shopping_list_with_grocy.py
@@ -32,7 +32,7 @@ class ShoppingListWithGrocyApi:
         self.mqtt_port = config.get("mqtt_port", 1883)
         self.mqtt_username = config.get("mqtt_username", None)
         self.mqtt_password = config.get("mqtt_password", None)
-        self.fetch_images = config.get("image_download_size", 0)
+        self.image_size = config.get("image_download_size", 0)
         self.ha_products = []
         self.final_data = []
         self.state_topic = "homeassistant/sensor/"
@@ -137,7 +137,7 @@ class ShoppingListWithGrocyApi:
     async def fetch_image(self, image_name: str):
         return await self.request(
             "get",
-            f"api/files/productpictures/{image_name}?force_serve_as=picture&best_fit_width={self.fetch_images}",
+            f"api/files/productpictures/{image_name}?force_serve_as=picture&best_fit_width={self.image_size}",
             "application/octet-stream",
         )
 
@@ -196,7 +196,7 @@ class ShoppingListWithGrocyApi:
                 self.ha_products.remove(entity)
 
             if (
-                self.fetch_images > 0
+                self.image_size > 0
                 and product_picture is not None
                 and product_picture != "null"
             ):
