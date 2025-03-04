3198cd824b7b5b1991c2e385c84cc5d0929f05e0
Benjamin Moreau <bmoreau1711@gmail.com>
2025-03-04 11:10:31 -0500

    fix: Fixed url validation regex issue (#47)

--- custom_components/shopping_list_with_grocy/config_flow.py
+++ custom_components/shopping_list_with_grocy/config_flow.py
@@ -193,10 +193,10 @@ class ShoppingListWithGrocyConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 def is_valid_url(url):
     regex = re.compile(
         r"^https?://"  # http:// or https://
-        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|"  # domain...
-        r"localhost|"  # localhost...
-        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
-        r"(?::\d+)?"  # optional port
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,}(?:\.[A-Z]{2,})?|"
+        r"localhost|"  # localhost
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # IP address
+        r"(?::\d+)?"  # Optional port
         r"(?:/?|[/?]\S+)$",
         re.IGNORECASE,
     )
